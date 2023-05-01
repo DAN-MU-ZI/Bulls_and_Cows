6ea6aa80fad94eb8635061813cf61acc43598ce6
rizingblare <rizingblare@gmail.com>
2023-05-01 20:03:40 +0900

    feat: compare two numbers
    입력받은 숫자와 컴퓨터 숫자를 서로 비교하는 기능 구현

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -1,4 +1,6 @@
-#import
+def compare_two_number():
+    pass
+
 
 # 게임을 실행하는 함수
 def play_game():
@@ -10,9 +12,9 @@ def play_game():
     # 사용자로부터 입력을 받습니다.
 
     # 입력이 유효한지 확인합니다.
-
+    
     # 숫자를 비교합니다.
-
+    compare_two_number()
     # 결과를 출력합니다.
 
 #play_game()
