9d005249f6c25ba509eb5dac0970576ebfe6b5a6
MilkTea24 <kimmanjae15@pusan.ac.kr>
2023-05-01 20:29:09 +0900

    feat: get_user_input() 메서드완성
    사용자의 입력을 받아서 입력이 유효한지 확인하고 결과를 int 배열로
    반환한다.

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -1,5 +1,29 @@
 #import
 
+def validate_input(user_input):
+        if not user_input.isdigit() or len(user_input) != 4:
+            print("유효하지 않은 입력입니다. 숫자 4개를 입력하세요.")
+            return False
+        elif len(set(user_input)) != 4:
+            print("중복된 숫자가 있습니다. 다시 입력해주세요.")
+            return False
+        return True
+
+
+def get_user_input():
+    user_input = input("중복되지 않는 숫자 4개를 입력해주세요:  ")
+    while not validate_input(user_input):
+        user_input = input("중복되지 않는 숫자 4개를 입력해주세요:  ")
+
+    number = int(user_input)
+    digits = list(str(number))
+    integer_array = [int(digit) for digit in digits]
+
+    return integer_array
+
+
+
+
 # 게임을 실행하는 함수
 def play_game():
     # 숫자를 뽑습니다.
@@ -8,6 +32,8 @@ def play_game():
     print("Let's play Bulls and Cows!")
 
     # 사용자로부터 입력을 받습니다.
+    
+    integer_array = get_user_input()
 
     # 입력이 유효한지 확인합니다.
 
